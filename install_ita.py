2b29efd989529f1cd1123847182bca5ea92e1f18
jotunn99 <diegrobot@hotmail.it>
2026-09-13 12:07:00 +0200

    Update install_ita.py

--- install_ita.py
+++ install_ita.py
@@ -6,7 +6,7 @@ Traduci l'interfaccia della tua stampante in Italiano in modo sicuro!
 import sys, os
 
 print("==========================================")
-print("  Snapmaker Italian GUI Patch - Mod v1.6  ")
+print("  Snapmaker Italian GUI Patch - Mod v1.7  ")
 print("==========================================\n")
 
 SRC  = '/usr/bin/gui'
